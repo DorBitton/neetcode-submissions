237168357202895e1cb9b605db014d4fd1087f24
Dor <dbitton01@gmail.com>
2026-06-24 18:11:56 +0300

    modify main.tf

--- terraform/IAC/main.tf
+++ terraform/IAC/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "your-name-terraform-state"
-    key            = "study/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
+    bucket       = "your-name-terraform-state"
+    key          = "study/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true   # replaces dynamodb_table
   }
 }
